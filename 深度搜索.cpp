693246ca8e54e7a4f6a3e7188eed4ffaf91f6478
Mr-Taufik <taozk4417@gmail.com>
2025-04-10 10:04:10 +0800

    更新 深度搜索.cpp

--- 深度搜索.cpp
+++ 深度搜索.cpp
@@ -54,3 +54,74 @@ void dfs(vector<vector<int>>& grid1, int x, int y, int m, int n)
      dfs(grid1, x, y + 1, m, n);
      dfs(grid1, x, y - 1, m, n);
 }
+
+
+广度搜索
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int answer(vector<vector<int>>& grid1);
+void bfs(vector<vector<int>>& grid1, int x, int y, int m, int n);
+
+int main()
+{
+    vector<vector<int>> grid1 = {
+        {1, 1, 1, 1, 0},
+        {1, 1, 0, 1, 0},
+        {1, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    cout << "岛屿数量: " << answer(grid1) << endl;
+    return 0;
+}
+
+int answer(vector<vector<int>>& grid1)
+{
+    int count = 0;
+    int m = grid1.size();
+    int n = grid1[0].size();
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(grid1[i][j] == 1)
+            {
+                count++;
+                bfs(grid1, i, j, m, n);
+            }
+        }
+    }
+    return count;
+}
+
+void bfs(vector<vector<int>>& grid1, int x, int y, int m, int n)
+{
+    queue<pair<int, int>> q;
+    q.push({x, y});
+    grid1[x][y] = 0; // 标记为已访问
+
+    // 四个方向：上 下 左 右
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+
+    while (!q.empty())
+    {
+        auto [curX, curY] = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int newX = curX + dx[i];
+            int newY = curY + dy[i];
+
+            if (newX >= 0 && newX < m && newY >= 0 && newY < n && grid1[newX][newY] == 1)
+            {
+                q.push({newX, newY});
+                grid1[newX][newY] = 0; // 标记为已访问
+            }
+        }
+    }
+}
